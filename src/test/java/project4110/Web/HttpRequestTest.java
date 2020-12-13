b7c78ca8c462b8734ba0294b4bb037421be4ac4d
Kelly Maisel <maiselkelly@gmail.com>
2020-12-13 18:26:53 -0500

    No Http test KM

--- src/test/java/project4110/Web/HttpRequestTest.java
+++ src/test/java/project4110/Web/HttpRequestTest.java
@@ -1,92 +1,92 @@
-package project4110.Web;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-//Tests if the server renders the website
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HttpRequestTest {
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Test
-    //This test is to see if the local host will render the website properly
-    public void homeShouldReturnDefaultMessage()throws Exception{
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/home",
-                String.class)).contains("<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-                "    <title>Roomiez</title>\n" +
-                "    <link rel=\"stylesheet\" href=\"/Images/stylesheet/style.css\">\n" +
-                "\n" +
-                "</head>\n" +
-                "\n" +
-                "<div class=\"header\" style=\"max-width:100%;overflow:auto;height:auto\">\n" +
-                "\n" +
-                "\n" +
-                "        <img class=\"roomiez\" src=\"/Images/images/Roomiez.png\"/>\n" +
-                "\n" +
-                "        <div class=\"links\">\n" +
-                "            <a href=\"https://www.facebook.com\"><img class=\"facebook\" src=\"/Images/images/facebook.png\"/></a>\n" +
-                "\n" +
-                "            <a href=\"https://www.twitter.com\"><img class=\"twitter\" src=\"/Images/images/twitter.png\"/></a>\n" +
-                "\n" +
-                "            <a href=\"https://www.instagram.com\"><img class=\"insta\" src=\"/Images/images/insta.jpeg\"/></a>\n" +
-                "\n" +
-                "\n" +
-                "    </div>\n" +
-                "</div>\n" +
-                "<br>\n" +
-                "<body>\n" +
-                "\n" +
-                "<div class=\"mainpagecontainer\">\n" +
-                "\n" +
-                "<div class=\"homeh1\">\n" +
-                "    <h1 style=\"color:black\">Find your roomie today!</h1>\n" +
-                "</div>\n" +
-                "\n" +
-                "\n" +
-                "<h2>Discover your potential roommate</h2>\n" +
-                "<h2>and newest friends here on <b>Roomiez</b></h2>\n" +
-                "<br>\n" +
-                "<br>\n" +
-                "<br>\n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "<div class=\"alignbutton\">\n" +
-                "<form method=\"get\" action=\"/Registration\">\n" +
-                "    <input type=\"submit\" class=\"button\" id=\"signupbutton\" value=\"Join Today!\" align=\"center\">\n" +
-                "</form>\n" +
-                "</div>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "    <br>\n" +
-                "\n" +
-                "\n" +
-                "</div>\n" +
-                "</body>\n" +
-                "</html>\n");
-    }
-
-}
-
-
+//package project4110.Web;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.web.server.LocalServerPort;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+////Tests if the server renders the website
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class HttpRequestTest {
+//
+//    @LocalServerPort
+//    private int port;
+//
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    @Test
+//    //This test is to see if the local host will render the website properly
+//    public void homeShouldReturnDefaultMessage()throws Exception{
+//        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/home",
+//                String.class)).contains("<!DOCTYPE html>\n" +
+//                "<html>\n" +
+//                "<head>\n" +
+//                "    <meta name=\"viewport\" content=\"width=device-width\">\n" +
+//                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+//                "    <title>Roomiez</title>\n" +
+//                "    <link rel=\"stylesheet\" href=\"/Images/stylesheet/style.css\">\n" +
+//                "\n" +
+//                "</head>\n" +
+//                "\n" +
+//                "<div class=\"header\" style=\"max-width:100%;overflow:auto;height:auto\">\n" +
+//                "\n" +
+//                "\n" +
+//                "        <img class=\"roomiez\" src=\"/Images/images/Roomiez.png\"/>\n" +
+//                "\n" +
+//                "        <div class=\"links\">\n" +
+//                "            <a href=\"https://www.facebook.com\"><img class=\"facebook\" src=\"/Images/images/facebook.png\"/></a>\n" +
+//                "\n" +
+//                "            <a href=\"https://www.twitter.com\"><img class=\"twitter\" src=\"/Images/images/twitter.png\"/></a>\n" +
+//                "\n" +
+//                "            <a href=\"https://www.instagram.com\"><img class=\"insta\" src=\"/Images/images/insta.jpeg\"/></a>\n" +
+//                "\n" +
+//                "\n" +
+//                "    </div>\n" +
+//                "</div>\n" +
+//                "<br>\n" +
+//                "<body>\n" +
+//                "\n" +
+//                "<div class=\"mainpagecontainer\">\n" +
+//                "\n" +
+//                "<div class=\"homeh1\">\n" +
+//                "    <h1 style=\"color:black\">Find your roomie today!</h1>\n" +
+//                "</div>\n" +
+//                "\n" +
+//                "\n" +
+//                "<h2>Discover your potential roommate</h2>\n" +
+//                "<h2>and newest friends here on <b>Roomiez</b></h2>\n" +
+//                "<br>\n" +
+//                "<br>\n" +
+//                "<br>\n" +
+//                "\n" +
+//                "\n" +
+//                "\n" +
+//                "<div class=\"alignbutton\">\n" +
+//                "<form method=\"get\" action=\"/Registration\">\n" +
+//                "    <input type=\"submit\" class=\"button\" id=\"signupbutton\" value=\"Join Today!\" align=\"center\">\n" +
+//                "</form>\n" +
+//                "</div>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "    <br>\n" +
+//                "\n" +
+//                "\n" +
+//                "</div>\n" +
+//                "</body>\n" +
+//                "</html>\n");
+//    }
+//
+//}
+//
+//
